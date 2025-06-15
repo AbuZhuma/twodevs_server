dd520d258b3726a5a34831470d6324a3849ef31d
AbuZhuma <abdirahman.zhumagulov@gmail.com>
2025-06-15 11:50:02 +0600

    save

--- src/controllers/news/createNews.js
+++ src/controllers/news/createNews.js
@@ -1,10 +1,14 @@
 const New = require("../../models/news");
+require("dotenv").config()
 
 const createNews = async (req, res) => {
       try {
             const body = req.body
-            if (!body.title || !body.date || !body.author || !body.description || !body.changelog) {
+            if (!body.title || !body.date || !body.author || !body.description || !body.changelog || !body.password) {
                   return res.status(400).json({ message: "Please check you`r fields!" })
+            }
+            if (process.env.NEWS_PASSWORD !== body.password) {
+                  return res.status(400).json({ message: "Wrong password!" })
             }
             const newNews = new New(body)
             newNews.save()
